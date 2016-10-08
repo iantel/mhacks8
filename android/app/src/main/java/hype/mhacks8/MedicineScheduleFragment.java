344344cb81ba67eaa2b3cc53fcba94a15306d218
Ian <ian.dn.tran@gmail.com>
2016-10-08 15:26:08 -0400

    Changed color scheme on login page and overall style

--- android/app/src/main/java/hype/mhacks8/MedicineScheduleFragment.java
+++ android/app/src/main/java/hype/mhacks8/MedicineScheduleFragment.java
@@ -9,14 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link MedicineScheduleFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link MedicineScheduleFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MedicineScheduleFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -27,7 +19,6 @@ public class MedicineScheduleFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    private OnFragmentInteractionListener mListener;
 
     public MedicineScheduleFragment() {
         // Required empty public constructor
@@ -67,13 +58,6 @@ public class MedicineScheduleFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_medicine_schedule, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
@@ -82,21 +66,6 @@ public class MedicineScheduleFragment extends Fragment {
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
