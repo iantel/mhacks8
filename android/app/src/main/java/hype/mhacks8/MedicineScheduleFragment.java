8e2af09ef47ca3ab9f46a2f9441546a448c3edf4
Ian <ian.dn.tran@gmail.com>
2016-10-08 18:50:30 -0400

    Added scrollable view to drug schedule fragment

--- android/app/src/main/java/hype/mhacks8/MedicineScheduleFragment.java
+++ android/app/src/main/java/hype/mhacks8/MedicineScheduleFragment.java
@@ -4,58 +4,34 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import hype.mhacks8.adapters.PrescriptionViewAdapter;
+
 
 public class MedicineScheduleFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    private RecyclerView prescriptionView;
 
     public MedicineScheduleFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MedicineScheduleFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MedicineScheduleFragment newInstance(String param1, String param2) {
-        MedicineScheduleFragment fragment = new MedicineScheduleFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_medicine_schedule, container, false);
+        View v = inflater.inflate(R.layout.fragment_medicine_schedule, container, false);
+        prescriptionView = (RecyclerView) v.findViewById(R.id.prescription_view);
+        PrescriptionViewAdapter adapter = new PrescriptionViewAdapter();
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this.getActivity());
+        prescriptionView.setAdapter(adapter);
+        prescriptionView.setLayoutManager(layoutManager);
+        return v;
     }
 
     @Override
